2fe37bf8c4211a7fc21be24acd10d49354b0e50c
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-10-12 07:56:40 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+   if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+    render :index
+    
+   end
+ end
+   
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated.error."
+    redirect_to book_path(params[:id])
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
